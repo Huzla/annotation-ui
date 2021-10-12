e3e353d7d16f657064c8ebe2a7ad0cc44ecb9807
laal <lari.alakukku@patria.fi>
2021-10-12 10:21:27 +0300

    Added selection and highlighting of css classes

--- src/app/annotation-document/annotation-document.component.ts
+++ src/app/annotation-document/annotation-document.component.ts
@@ -1,6 +1,9 @@
 import { Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { Observable, Subscription } from 'rxjs';
 
+const focusHighlightClass: string = 'focused-item';
+const selectedClass: string = 'selected-item';
 
 @Component({
   selector: 'app-annotation-document',
@@ -15,23 +18,92 @@ export class AnnotationDocumentComponent implements OnInit {
   @Input() documentUrl: string = '';
   _safeUrl: SafeResourceUrl = '';
 
+  @Input() classInFocus!: Observable<string>;
+  @Input() classAdd!: Observable<string>;
+  @Input() classDelete!: Observable<string>;
+
   @Output()
   classesEvent = new EventEmitter<Set<string>>();
+
+  private subscriptions: Subscription[] = [];
 
   constructor(private sanitizer: DomSanitizer) { }
 
   ngOnInit(): void {
     this._safeUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.documentUrl);
 
-    setTimeout(() => {
-      console.log(this.iframe);
+    this.subscriptions.push(this.classInFocus.subscribe((className: string) => {
+      this.removeClassFromIframe(focusHighlightClass, focusHighlightClass);
+
+      this.addClassToIframe(className, focusHighlightClass);
       
-      this.classesEvent.emit(
-        new Set<string>(
-        Array.from(this.iframe.nativeElement.contentDocument.querySelectorAll("*") as Array<HTMLElement>)
-        .map((element: HTMLElement) => element.className))
-        );
-    }, 100);
+      this.scrollTo(className);
+    }));
+
+    this.subscriptions.push(this.classAdd.subscribe((className: string) => {
+      this.addClassToIframe(className, selectedClass);
+    }));
+
+    this.subscriptions.push(this.classDelete.subscribe((className: string) => {
+      this.removeClassFromIframe(className, selectedClass);
+    }));
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.forEach(sub => sub.unsubscribe());
+  }
+
+  private removeClassFromIframe(target: string, className: string): void {
+    this.iframe.nativeElement.contentDocument
+        .querySelectorAll(this.stringToClassSelector(target))
+        .forEach((element: HTMLElement) => {
+          element.classList.remove(className);
+        });
+  }
+
+  // Note that target can consist of multiple classes.
+  private addClassToIframe(target: string, newClassName: string): void {
+    this.iframe.nativeElement.contentDocument
+        .querySelectorAll(this.stringToClassSelector(target))
+        .forEach((element: HTMLElement) => {
+          element.classList.add(newClassName);
+        });
+  }
+
+  private stringToClassSelector(className: string) {
+    return '.' + className.replace(/ /gi, '.');
+  }
+
+  private scrollTo(target: string) {
+    (this.iframe.nativeElement.contentDocument.querySelector(this.stringToClassSelector(target)) as Element).scrollIntoView(true);
+  }
+
+  outputClasses(): void {
+    const cssString = `
+      .focused-item {
+          border: 1rem solid #00FFFF;
+          border-radius: 0.2rem;
+          filter: invert(100%);
+      }
+
+      .selected-item {
+        border: 1rem solid #00FF00;
+        border-radius: 0.2rem;
+        background-color: #00FF00;
+      }
+    `;
+
+    const styleElem = this.iframe.nativeElement.contentDocument.createElement('style');
+    styleElem.appendChild(this.iframe.nativeElement.contentDocument.createTextNode(cssString));
+
+    this.iframe.nativeElement.contentDocument.head.appendChild(styleElem);
+
+    this.classesEvent.emit(
+      new Set<string>(
+      Array.from(this.iframe.nativeElement.contentDocument.querySelectorAll("*") as Array<HTMLElement>)
+      .map((element: HTMLElement) => element.getAttribute('class') || '')
+      .filter((s: string) => s !== ''))
+      );
   }
 
 }
--- src/app/annotation/annotation.component.ts
+++ src/app/annotation/annotation.component.ts
@@ -1,5 +1,6 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subject } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Annotation } from '../annotation';
 import { AnnotationService } from '../annotation.service';
@@ -16,14 +17,18 @@ export class AnnotationComponent implements OnInit {
 
   groups: number[] = [];
   selectedGroup: number = 0;
+  selectedClasses: Set<string> = new Set();
   documentUrl: string = '';
 
   annotation?: Annotation;
 
-  @ViewChild('documentViewer')
-  documentElement!: ElementRef;
+  classInFocus?: string;
+  classInFocusSubject: Subject<string> = new Subject<string>();
+  classAddSubject: Subject<string> = new Subject<string>();
+  classDeleteSubject: Subject<string> = new Subject<string>();
 
-  cssClasses: Set<string> = new Set(); 
+
+  cssClasses: Set<string> = new Set();
 
   constructor(private route: ActivatedRoute,
     private annotationService: AnnotationService,
@@ -38,22 +43,58 @@ export class AnnotationComponent implements OnInit {
     this.getAnnotation();
   }
 
-  private async getAnnotation() {
+  private async getAnnotation(): Promise<void> {
     this.annotation = await this.annotationService
       .getAnnotationById(this.domainId, this.annotationId)
       .toPromise();
+
+    if (this.annotation.classes.length) {
+      this.selectedClasses = new Set(this.annotation.classes);  
+    }
+  
   }
 
-  private async getDomain() {
+  private async getDomain(): Promise<void> {
     this.groups = Array.from(Array((await this.domainService.getDomainById(this.domainId).toPromise()).groups).keys());
   }
 
-  selectGroup(group: number) {
+  selectGroup(group: number): void {
     this.selectedGroup = group;
   }
 
-  updateClassList(classes: Set<string>) {
+  updateClassList(classes: Set<string>): void {
     this.cssClasses = classes;
+  }
+
+  focusOnClass(c: string): void {
+    if (this.classInFocus !== c) {
+      this.classInFocus = c;
+      this.classInFocusSubject.next(c);
+    }
+    
+  }
+
+  toggleSelection(c: string): void {
+    if (this.selectedClasses.has(c)) {
+      this.selectedClasses.delete(c);
+      this.classDeleteSubject.next(c);
+    }
+    else {
+      this.selectedClasses.add(c);
+      this.classAddSubject.next(c);
+    }
+  }
+
+  determineClassColor(c: string): string {
+    if (this.selectedClasses.has(c)) {
+      return 'success';
+    }
+    else if (c === this.classInFocus) {
+      return 'danger';
+    }
+    else {
+      return 'secondary';
+    }
   }
 
 }
